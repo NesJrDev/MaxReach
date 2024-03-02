75c8718a74e126c06f39b2e8febd92d6c7b0c7a5
NesJrDev <149549343+NesJrDev@users.noreply.github.com>
2024-03-02 15:27:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert(window.innerWidth)
+let label = document.querySelector(".label-input")
+let input = document.querySelector(".input-input")[0]
+
+input.value = "pepepepote"
+input[0].value = "elmascrack"
